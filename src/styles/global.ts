3be364ea0780225708e1d6ee2bdd62357badd87b
Dyego Soriano <dyegodf2@gmail.com>
2022-03-07 22:04:46 -0300

    style: config global styles

---
+++ src/styles/global.ts
@@ -0,0 +1,43 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --background: #F0F2F5;
+    --blue-light: #6933FF;
+    --text-title: #363F5F;
+    --text-body: #969CB3;
+    --shape: #FFFFFF;
+    --blue: #5429CC;
+    --red: #E52E4D;
+  }
+
+  * {
+    box-sizing: border-box;
+    padding: 0;
+    margin: 0;
+  }
+
+  html {
+    @media (max-width: 1080px) {
+      font-size: 93.75%;
+    }
+
+    @media (max-width: 720px) {
+      font-size: 85.5%;
+    }
+  }
+
+  body {
+    -webkit-font-smoothing: antialiased;
+    background: var(--background);
+  }
+
+  button {
+    cursor: pointer;
+  }
+
+  [disabled] {
+    cursor: not-allowed;
+    opacity: 0.6;
+  }
+`
